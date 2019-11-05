4eaf187d32f7290207c33049890e3abebe5165bd
yuhongjing <1240945408@qq.com>
2019-11-05 21:50:46 +0800

    add new notebook

---
+++ docs/.vuepress/theme/NavLink.vue
@@ -0,0 +1,49 @@
+<template>
+  <router-link
+    v-if="!isExternal(link)"
+    :to="link"
+    :exact="exact"
+    class="nav-link"
+  >{{ item.text }}</router-link>
+  <a
+    v-else
+    :href="link"
+    :target="isMailto(link) || isTel(link) ? null : '_blank'"
+    :rel="isMailto(link) || isTel(link) ? null : 'noopener noreferrer'"
+    class="nav-link external"
+  >
+    {{ item.text }}
+    <OutboundLink/>
+  </a>
+</template>
+
+<script>
+import {isExternal, isMailto, isTel, ensureExt} from './util';
+
+export default {
+    props: {
+        item: {
+            required: true
+        }
+    },
+
+    computed: {
+        link () {
+            return ensureExt(this.item.link);
+        },
+
+        exact () {
+            if (this.$site.locales) {
+                return Object.keys(this.$site.locales).some(rootLink => rootLink === this.link);
+            }
+            return this.link === '/';
+        }
+    },
+
+    methods: {
+        isExternal,
+        isMailto,
+        isTel
+    }
+};
+</script>
---
+++ docs/.vuepress/theme/SidebarGroup.vue
@@ -0,0 +1,76 @@
+<template>
+  <div
+    :class="{ first, collapsable }"
+    class="sidebar-group"
+  >
+    <p
+      :class="{ open }"
+      class="sidebar-heading"
+      @click="$emit('toggle')"
+    >
+      <span>{{ item.title }}</span>
+      <span
+        v-if="collapsable"
+        :class="open ? 'down' : 'right'"
+        class="arrow"/>
+    </p>
+
+    <DropdownTransition>
+      <ul
+        v-if="open || !collapsable"
+        ref="items"
+        class="sidebar-group-items"
+      >
+        <li v-for="child in item.children">
+          <SidebarLink :item="child"/>
+        </li>
+      </ul>
+    </DropdownTransition>
+  </div>
+</template>
+
+<script>
+import SidebarLink from './SidebarLink.vue';
+import DropdownTransition from './DropdownTransition.vue';
+
+export default {
+    name: 'SidebarGroup',
+    components: {SidebarLink, DropdownTransition},
+    props: ['item', 'first', 'open', 'collapsable']
+};
+</script>
+
+<style lang="stylus">
+.sidebar-group
+  &:not(.first)
+    margin-top 1em
+  .sidebar-group
+    padding-left 0.5em
+  &:not(.collapsable)
+    .sidebar-heading
+      cursor auto
+      color inherit
+
+.sidebar-heading
+  color #999
+  transition color .15s ease
+  cursor pointer
+  font-size 1.1em
+  font-weight bold
+  // text-transform uppercase
+  padding 0 1.5rem
+  margin-top 0
+  margin-bottom 0.5rem
+  &.open, &:hover
+    color inherit
+  .arrow
+    position relative
+    top -0.12em
+    left 0.5em
+  &:.open .arrow
+    top -0.18em
+
+.sidebar-group-items
+  transition height .1s ease-out
+  overflow hidden
+</style>
